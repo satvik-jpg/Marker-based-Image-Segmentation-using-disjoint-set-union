3551a36aecf61f9efc93950a3a2f0242e45ddd79
satvik6391765316 <127044449+satvik6391765316@users.noreply.github.com>
2024-01-07 03:08:20 +0530

    Removed obsolete files

--- IGNORED/display_org_image.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-import cv2
-import numpy as np
-
-input_image_path = "example.jpg"
-img = plt.imread(input_image_path)
-plt.imshow(img)
-plt.show()
